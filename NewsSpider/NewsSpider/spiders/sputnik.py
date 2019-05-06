9b23f33d99f644d73139ed1025ddfb756480b0b2
TonyBay <herbertyi@outlook.com>
2019-05-07 06:25:44 +0800

    add sputnik news

---
+++ NewsSpider/NewsSpider/spiders/sputnik.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import time
+from NewsSpider.items import NewsItem
+class SputnikSpider(scrapy.Spider):
+    name = 'sputnik'
+    allowed_domains = ['sputniknews.cn']
+    start_urls = ['http://sputniknews.cn/politics/']
+
+    def start_requests(self):
+        yield scrapy.Request(
+            # url='http://sputniknews.cn/politics/' + time.strftime("%Y%m%d", time.localtime()),
+            url='http://sputniknews.cn/politics/20190503',
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        item = NewsItem()
+        for new in response.xpath('//div[@class="b-plainlist__info"]'):
+            item['title'] = new.xpath('./h2/a/text()').extract()[0]
+            item['link'] = 'http://sputniknews.cn' + new.xpath('./h2/a/@href').extract()[0]
+            item['site'] = 'sputnik'
+            yield item
+        try:
+            yield scrapy.Request(
+                url='http://sputniknews.cn' + response.xpath('//div[@class="b-more"]/a/@data-href').extract()[0],
+                callback=self.parse
+            )
+        except:
+            pass
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 + [澎湃新闻](https://www.thepaper.cn/)
 + [界面新闻](https://www.jiemian.com/)
++ [俄罗斯卫星通讯社](http://sputniknews.cn/)
